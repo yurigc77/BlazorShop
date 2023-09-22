5d917bb5d14959792daac59fe1170e8290f9bfac
Yuri Garcia Campos <yurigc@yahoo.com.br>
2023-09-22 13:17:47 -0300

    repository

---
+++ BlazorShop.Api/Repositories/IProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using BlazorShop.Api.Entities;
+
+namespace BlazorShop.Api.Repositories;
+
+public interface IProdutoRepository
+{
+    Task<IEnumerable<Produto>> GetItens();
+    Task<Produto> GetItem(int id);
+
+    Task<IEnumerable<Produto>> GetItensPorCategoria(int id);
+    Task<IEnumerable<Categoria>> GetCategorias();
+}
