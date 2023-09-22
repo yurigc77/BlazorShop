ff51c5b873a76772e23ded425f9f07028568c32f
Yuri Garcia Campos <yurigc@yahoo.com.br>
2023-09-22 13:18:35 -0300

    definição de url do projeto web com a url a api

--- BlazorShop.Web/Program.cs
+++ BlazorShop.Web/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = "https://localhost:7225";
+
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(baseUrl)//builder.HostEnvironment.BaseAddress) 
+});
 
 await builder.Build().RunAsync();
