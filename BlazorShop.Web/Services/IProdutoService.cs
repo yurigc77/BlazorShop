d5d64cbe91cd0512c42126224a1bc7ffd4f64626
Yuri Garcia Campos <yurigc@yahoo.com.br>
2023-09-25 10:04:26 -0300

    produto service

---
+++ BlazorShop.Web/Services/IProdutoService.cs
@@ -0,0 +1,12 @@
+﻿using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.Web.Services;
+
+public interface IProdutoService
+{
+    Task<IEnumerable<ProdutoDto>> GetItens();
+    Task<ProdutoDto> GetItem(int id);
+
+    //Task<IEnumerable<CategoriaDto>> GetCategorias();
+    //Task<IEnumerable<ProdutoDto>> GetItensPorCategoria(int categoriaId);
+}
---
+++ BlazorShop.Web/Services/ProdutoService.cs
@@ -0,0 +1,60 @@
+﻿using BlazorShop.Models.DTOs;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace BlazorShop.Web.Services;
+
+public class ProdutoService : IProdutoService
+{
+    public HttpClient _httpClient;
+    public ILogger<ProdutoService> _logger;
+
+    public ProdutoService(HttpClient httpClient,ILogger<ProdutoService> logger)
+    {
+        _httpClient = httpClient;
+        _logger = logger;
+    }
+
+    public async Task<IEnumerable<ProdutoDto>> GetItens()
+    {
+        try
+        {
+            var produtosDto = await _httpClient.GetFromJsonAsync<IEnumerable<ProdutoDto>>("api/produtos");
+            return produtosDto;
+        }
+        catch (Exception)
+        {
+            _logger.LogError("Erro ao acessar produtos : api/produtos ");
+            throw;
+        }
+    }
+
+    public async Task<ProdutoDto> GetItem(int id)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"api/produtos/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return default(ProdutoDto);
+                }
+                return await response.Content.ReadFromJsonAsync<ProdutoDto>();
+            }
+            else
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                _logger.LogError($"Erro a obter produto pelo id= {id} - {message}");
+                throw new Exception($"Status Code : {response.StatusCode} - {message}");
+            }
+        }
+        catch (Exception)
+        {
+            _logger.LogError($"Erro a obter produto pelo id={id}");
+            throw;
+        }
+    }
+
+}
