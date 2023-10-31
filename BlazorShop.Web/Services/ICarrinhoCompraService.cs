f49d07676c67bb0303dd893d1ebdf82f6d01539a
Yuri Garcia Campos <yurigc@yahoo.com.br>
2023-10-31 09:48:50 -0300

    carrinho de compras

---
+++ BlazorShop.Web/Services/ICarrinhoCompraService.cs
@@ -0,0 +1,14 @@
+﻿using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.Web.Services;
+
+public interface ICarrinhoCompraService
+{
+    Task<List<CarrinhoItemDto>> GetItens(string usuarioId);
+    Task<CarrinhoItemDto> AdicionaItem(CarrinhoItemAdicionaDto carrinhoItemAdicionaDto);
+    //Task<CarrinhoItemDto> DeletaItem(int id);
+    //Task<CarrinhoItemDto> AtualizaQuantidade(CarrinhoItemAtualizaQuantidadeDto carrinhoItemAtualizaQuantidadeDto);
+
+    //event Action<int> OnCarrinhoCompraChanged;
+    //void RaiseEventOnCarrinhoCompraChanged(int totalQuantidade);
+}
